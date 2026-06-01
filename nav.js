bdd308448043d7e9965b0f0f3e14707de8a83abc
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-06-01 16:42:45 +0545

    removed one bug

--- nav.js
+++ nav.js
@@ -1,16 +1,35 @@
-document.querySelectorAll('.dropdown-btn').forEach(btn =>{
-    btn.addEventListener('click',() =>{
-        const subMenu =btn.nextElementSibling; // this selects the ul.submenu right after the menu
-        subMenu.classList.toggle('show');
-        btn.querySelector('.dropdown-arrow').classList.toggle('rotated');
-    });
-});
-//for togglebtn
+
 const togglebtn =document.getElementById('toggle-btn');
 const sidebar =document.getElementById('sidebar');
+
+
+
+//for dropdown of the sub-menu
+document.querySelectorAll('.dropdown-btn').forEach(btn =>{
+    btn.addEventListener('click',() =>{
+
+        if(sidebar.classList.contains('close')){
+        sidebar.classList.remove('close');
+        togglebtn.classList.remove('rotated')
+    }
+    const subMenu =btn.nextElementSibling;
+    subMenu.classList.toggle('show');
+    btn.querySelector('.dropdown-arrow').classList.toggle('rotated');
+    });
+});
 
 togglebtn.addEventListener('click' ,() =>{
     togglebtn.classList.toggle('rotated');
     sidebar.classList.toggle('close');
-    
+
+    //
+    if(sidebar.classList.contains('close')){
+        document.querySelectorAll('.sub-menu').forEach(menu =>{
+            menu.classList.remove('show');
+        });
+        document.querySelectorAll('.dropdown-arrow').forEach(arrow =>{
+            arrow.classList.remove('rotated');
+        });
+    }
 });
+
